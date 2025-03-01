8062f32affdedf332675d70b4d26c91dcbfc5690
nikhilgoud003 <nikhilgoud2001@gmail.com>
2025-02-28 22:56:38 -0500

    Initial commit for task manager application

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TaskManagerApp());
+}
+
+class TaskManagerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: TaskListScreen(),
+    );
+  }
+}
