2bac84a1a287b290fe2f6fed0ddae2b577e9d77e
nikhilgoud003 <nikhilgoud2001@gmail.com>
2025-02-28 22:58:36 -0500

    Implemented task management with add, toggle, and delete functions and added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,46 @@ class TaskManagerApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home: TaskListScreen(),
+      home: TaskListScreen(),
     );
   }
 }
+class Task {
+  String name;
+  bool isCompleted;
+  String priority;
+
+  Task({required this.name, this.isCompleted = false, required this.priority});
+}
+
+class TaskListScreen extends StatefulWidget {
+  @override
+  _TaskListScreenState createState() => _TaskListScreenState();
+}
+
+class _TaskListScreenState extends State<TaskListScreen> {
+  final List<Task> _tasks = [];
+  final TextEditingController _taskController = TextEditingController();
+  final TextEditingController _searchController = TextEditingController();
+  String _selectedPriority = 'Medium';
+
+  void _addTask() {
+    if (_taskController.text.isNotEmpty) {
+      setState(() {
+        _tasks.add(Task(name: _taskController.text, priority: _selectedPriority));
+        _taskController.clear();
+      });
+    }
+  }
+
+  void _toggleTaskCompletion(int index) {
+    setState(() {
+      _tasks[index].isCompleted = !_tasks[index].isCompleted;
+    });
+  }
+
+  void _deleteTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+  }
